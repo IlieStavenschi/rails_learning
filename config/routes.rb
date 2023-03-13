95c8aa3140f9d663681386f3e9a6491035a1400e
Ilie Stavenschi <istavenschi@gmail.com>
2023-03-13 12:47:15 +0200

    New page About

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
 
   # Defines the root path route ("/")
   # root "articles#index"
